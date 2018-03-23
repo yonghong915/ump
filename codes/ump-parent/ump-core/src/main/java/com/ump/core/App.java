e2c4c890ce6dba00b6882ebe678b018c0d68f79a
fangyh <13684088503@139.com>
2018-03-23 23:19:50 +0800

    build web environment

--- codes/ump-parent/ump-core/src/main/java/com/ump/core/App.java
+++ codes/ump-parent/ump-core/src/main/java/com/ump/core/App.java
@@ -4,10 +4,8 @@ package com.ump.core;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
---
+++ codes/ump-parent/ump-core/src/main/java/com/ump/core/runtime/UmpRuntimeListener.java
@@ -0,0 +1,7 @@
+package com.ump.core.runtime;
+
+import org.springframework.web.context.ContextLoaderListener;
+
+public class UmpRuntimeListener extends ContextLoaderListener {
+
+}
