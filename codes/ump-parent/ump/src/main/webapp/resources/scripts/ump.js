ba4b4bb3925030f493409ba0f35f1d63c7be3792
fangyh <13684088503@139.com>
2018-03-30 23:27:57 +0800

    protobuf grpc function

--- codes/ump-parent/ump/src/main/webapp/resources/scripts/ump.js
+++ codes/ump-parent/ump/src/main/webapp/resources/scripts/ump.js
@@ -7,26 +7,27 @@ var globalContent = new Array();
 ump.loadAuthResList = function() {
 	var path = 'ump';
 	// /auth/loadAuth.do
-//	$.ajax({
-//		type : "GET", // 请求类型
-//		url : path + "/auth/loadAuth.do", // URL
-//		data : {
-//			uid : ''
-//		}, // 传递的参数
-//		dataType : "json", // 返回的数据类型
-//		success : function(data) { // data就是返回的json类型的数据
-//			var result = obj.data;
-//			globalContent[AUTH_RES_LIST] = result;
-//		},
-//		error : function(data) {
-//			alert("删除失败");
-//		}
-//	});
-	globalContent[AUTH_RES_LIST] = ['.do'];
-	//alert('globalContent=' + globalContent[AUTH_RES_LIST]);
+	// $.ajax({
+	// type : "GET", // 请求类型
+	// url : path + "/auth/loadAuth.do", // URL
+	// data : {
+	// uid : ''
+	// }, // 传递的参数
+	// dataType : "json", // 返回的数据类型
+	// success : function(data) { // data就是返回的json类型的数据
+	// var result = obj.data;
+	// globalContent[AUTH_RES_LIST] = result;
+	// },
+	// error : function(data) {
+	// alert("删除失败");
+	// }
+	// });
+	var globalContent = this.getGlobalContext();
+	globalContent[AUTH_RES_LIST] = [ 'test.do','aaaaa.do' ];
+	// alert('globalContent=' + globalContent[AUTH_RES_LIST]);
 }
 
-ump.getGlobalContent = function() {
+ump.getGlobalContext = function() {
 	return this.findPropertyValue(window, GLOBAL_CONTEXT);
 }
 ump.findPropertyValue = function(win, propName) {
@@ -43,26 +44,35 @@ ump.isTop = function(win) {
 }
 
 ump.applyResAuth = function(authResList) {
-	var appid = $("#myDiv").data("appid"); //123
-	var appId = $("#myDiv").data("app-id"); //456
-	alert(appid+'  '+appId);
-	
+	var appid = $("#myDiv").data("appid"); // 123
+	var appId = $("#myDiv").data("app-id"); // 456
+	// alert(appid + ' ' + appId);
+
 	$('*[data-resid]').each(function() {
 		var $this = $(this);
 		var resIdAttr = $this.attr('data-resid');
 		var flag = false;
-		if (resIdAttr == authResList) {
-			flag = true;
+		for (var i = 0; i < authResList.length; i++) {
+			if (resIdAttr == authResList[i]) {
+				flag = true;
+			}
 		}
+		alert('flag='+flag);
 		if (!flag) {
-			$this.remove();
-			//$this.attr('readonly', true);
+			// $this.remove();
+			$this.attr('disabled', true);
 		}
 	});
 }
+
+ump.getAuthResList = function() {
+	var authResList = this.getGlobalContext();
+	alert(authResList[AUTH_RES_LIST]);
+	return authResList ? authResList[AUTH_RES_LIST] : null;
+}
 ump.parse = function() {
 	if ($J.enableResAuth) {
-		var authResList = this.getGlobalContent();
+		var authResList = this.getAuthResList();
 		alert('authResList=' + authResList);
 		this.applyResAuth(authResList);
 	}
