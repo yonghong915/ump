58c7b2fe21be172c99b82ef2644f12149a9e009e
fangyh <13684088503@139.com>
2018-07-18 16:04:33 +0800

    springboot eureka优化

--- codes/ump-parent/ump-springboot/ump-config/src/main/java/com/ump/config/ConfigApplication.java
+++ codes/ump-parent/ump-springboot/ump-config/src/main/java/com/ump/config/ConfigApplication.java
@@ -1,11 +1,9 @@
-package org.ump.config;
+package com.ump.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 
@@ -14,8 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
-@EnableAutoConfiguration
-@EnableEurekaClient 
+// @EnableAutoConfiguration
 public class ConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
--- codes/ump-parent/ump-springboot/ump-workflow/src/main/java/com/ump/workflow/WorkflowApplication.java
+++ codes/ump-parent/ump-springboot/ump-workflow/src/main/java/com/ump/workflow/WorkflowApplication.java
@@ -1,29 +1,17 @@
 package com.ump.workflow;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 
  *
  */
 @SpringBootApplication
-@EnableEurekaClient
-@RestController
+@EnableDiscoveryClient
 public class WorkflowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkflowApplication.class, args);
-	}
-
-	@Value("${workname}") // git配置文件里的key
-	String myww;
-
-	@RequestMapping(value = "/hi")
-	public String hi() {
-		return myww;
 	}
 }
